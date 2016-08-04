10ebd19d29cdd262e216f5c75fcbff459911ae40
yummyHit <crazyboyi@naver.com>
2016-08-04 22:09:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,13 +10,36 @@ Must be add linker: ws2_32.lib; wpcap.lib; Packet.lib
 #include <stdlib.h>
 #include <WinSock2.h>
 #include <pcap.h>
-#include <libnet-headers.h>
+#include "libnet-headers.h"
+
+void mac_print(int init, struct libnet_ethernet_hdr *eth) {
+	int i = 0;
+	if(init == 0) {
+		while(i < ETHER_ADDR_LEN) {
+			printf("%02x:", eth->ether_shost[i]);	// print Source Mac Address
+			if((i+1) == (ETHER_ADDR_LEN-1))		// If next i value is last index, print last address value and quit loop.
+				printf("%02x", eth->ether_shost[++i]);
+			i++;
+		}
+		printf("\n");
+	}
+	else {
+		i = 0;
+		while(i < ETHER_ADDR_LEN) {
+			printf("%02x:", eth->ether_dhost[i]);	// print Destination Mac Address
+			if((i+1) == (ETHER_ADDR_LEN-1))		// If next i value is last index, print last address value and quit loop.
+				printf("%02x", eth->ether_dhost[++i]);
+			i++;
+		}
+		printf("\n");
+	}
+}
 
 int main()
 {
-	int num, serv_sock, i = 0;	// num variable get interface number, i variable is only integer variable.
+	int num, i = 0;	// num variable get interface number, i variable is only integer variable.
+	char str[INET_ADDRSTRLEN];	// This variable used by inet_ntop() function.
 	u_int pktCnt = 0;	// Packet Count variable.
-	pcap_if_t *alldevs, *ex;	// alldevs pointer variable is devices search, ex pointer variable is descript each device.
 	struct pcap_pkthdr *header;
 	const u_char *data;			// data pointer variable will have packet content after pcap_next
 	struct libnet_ethernet_hdr *eth;
@@ -25,52 +48,20 @@ int main()
 	struct libnet_tcp_hdr *tcp;
 	struct libnet_udp_hdr *udp;	// Each of struct pointer variable.
 	//char file[] = "httpGet.pcap";
-	//char file[] = "httpEx.pcap";
-	char file[] = "httpPost.pcap";
+	char file[] = "httpEx.pcap";
+	//char file[] = "httpPost.pcap";
 
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t *pcap = pcap_open_offline(file, errbuf);	// pcap file open!
 	
-	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1)
-    {
-        fprintf(stderr,"Error in pcap_findalldevs: %s\n", errbuf);
-        exit(1);
-    }
-
-	for(ex = alldevs; ex; ex=ex->next)
-    {
-        printf("%d. %s", ++i, ex->name);
-        if (ex->description)
-            printf(" (%s)\n", ex->description);
-        else
-            printf(" (No description available)\n");
-    }
-    
-    if(i == 0)
-    {
-        printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
-        return -1;
-    }
-    
-    printf("Enter the interface number (1-%d):", i);
-    scanf_s("%d", &num);
-
-	if(num < 1 || num > i)
-    {
-        printf("\nInterface number out of range.\n");
-        pcap_freealldevs(alldevs);
-        return -1;
-    }
-
-	while((num = pcap_next_ex(pcap, &header, &data)) >= 0) {
-		eth = (struct libnet_ethernet_hdr *) data;	// eth point to packet's first part.
+	while((num = pcap_next_ex(pcap, &header, &data)) > 0) {
+		eth = (struct libnet_ethernet_hdr*) data;	// eth point to packet's first part.
 		if(ntohs(eth->ether_type) == ETHERTYPE_IP)	// IPv4 is '0800' in packet. So check this.
 			ip = (struct libnet_ipv4_hdr*) (data + sizeof(struct libnet_ethernet_hdr));	// ip point to part of packet in sequence.
 		else if(ntohs(eth->ether_type) == ETHERTYPE_ARP)	// arp is '0806' in packet.
 			arp = (struct libnet_arp_hdr*) (data + sizeof(struct libnet_ethernet_hdr));
 		else {
 			printf("\nIP Error. Please Check ethernet frame.\nCurrent Type : %04x", ntohs(eth->ether_type));
-			pcap_freealldevs(alldevs);
 			return -1;
 		}
 		if(ip->ip_p == IPPROTO_TCP)	// If tcp protocol, continue it.
@@ -79,36 +70,20 @@ int main()
 			udp = (struct libnet_udp_hdr*) (data + sizeof(struct libnet_ethernet_hdr) + sizeof(struct libnet_ipv4_hdr));
 		else {
 			printf("\nTCP Error. Please Check ip packet.\nCurrent protocol : %02x", ip->ip_p);
-			pcap_freealldevs(alldevs);
 			return -1;
 		}
 		
 		printf("Packet No .%i\n", ++pktCnt);	// print packet number
 		printf("Packet size : %d bytes\n", header->len);	// print packet length
-		printf("MAC src: "); i = 0;
-		while(i < ETHER_ADDR_LEN) {
-			printf("%02x:", eth->ether_shost[i]);	// print Source Mac Address
-			if((i+1) == (ETHER_ADDR_LEN-1))		// If next i value is last index, print last address value and quit loop.
-				printf("%02x", eth->ether_shost[++i]);
-			i++;
-		}
-		printf("\n"); i = 0;
-		printf("MAC dest: ");
-		while(i < ETHER_ADDR_LEN) {
-			printf("%02x:", eth->ether_dhost[i]);	// print Destination Mac Address
-			if((i+1) == (ETHER_ADDR_LEN-1))		// If next i value is last index, print last address value and quit loop.
-				printf("%02x", eth->ether_dhost[++i]);
-			i++;
-		}
-		printf("\n"); i = 0;	
-		printf("IP src : %s\n", inet_ntoa(ip->ip_src));		// print Source IP Address
-		printf("IP dest : %s\n", inet_ntoa(ip->ip_dst));	// print Destination IP Address
+		printf("MAC src: ");	mac_print(i++, eth);
+		printf("MAC dest: ");	mac_print(i, eth);
+		printf("IP src : %s\n", inet_ntop(AF_INET, &ip->ip_src.S_un.S_addr, str, sizeof(str)));		// print Source IP Address
+		printf("IP dest : %s\n", inet_ntop(AF_INET, &ip->ip_dst.S_un.S_addr, str, sizeof(str)));	// print Destination IP Address
 		printf("Src port : %d\n", ntohs(tcp->th_sport));	// print Source TCP Port
 		printf("Dst port : %d\n", ntohs(tcp->th_dport));	// print Destination TCP Port
 
 		if(header->len != header->caplen)
 			printf("Capture size error. Different size : %ld bytes\n", header->len);
-				
 		printf("\n\n");
 	}
     return 0;
